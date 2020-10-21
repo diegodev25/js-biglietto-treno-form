f78879499b055863d83717f1293a030539d189ae
Diego <sturbadiego25@outlook.it>
2020-10-21 15:49:56 +0200

    Struttura base dell'esercizio su html e css

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// calcolo del prezzo del biglietto del treno con form
+
+// Codice treno (numero casuale tra 90000 e 100000 escluso)
+// Numero carrozza
+// Prezzo calcolato
+// Categoria selezionata dall’utente
+
+// Nome passeggero

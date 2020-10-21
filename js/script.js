8bed6df2adc18c19eb9edcf6f1ba3bc8d4d59c28
Diego <sturbadiego25@outlook.it>
2020-10-21 17:45:30 +0200

    Regole js per la generaazione prezzo del biglietto

--- js/script.js
+++ js/script.js
@@ -1,30 +1,33 @@
-// calcolo del prezzo del biglietto del treno con form
-
-// nome passeggero
-// var name = document.getElementById('nome-passeggero');
-// console.log("Il tuo nome è " name);
-// // km da percorrere
-// var km = document.getElementById('chilometri');
-// console.log("I km che vuoi percorrere sono ", km)
-//
-// // fascie d'età
-// var age = document.getElementById('eta');
-// console.log("La tua fascia di età è ", age)
-//
-// //prezzo di base del biglietto definito in base ai km (0.21 € al km)
-var priceDef = (0.21 * km);
-// console.log("Il prezzo base è", price);
-
-// Condizioni di sconto per minorenni e over 65
-// if (age < 18) {
-//   priceDef = priceDef * 0.80
-//   console.log("Il prezzo di del biglietto per i minorenni è " priceDef);
-// } else if (age > 65) {
-//   priceDef = priceDef * 0.60
-//   console.log("Il prezzo di del biglietto per gli over 65 è " priceDef);
+// Genera i risultai del biglietto tramite js
 var generate = document.getElementById('check');
 generate.addEventListener('click',
   function () {
-    alert(priceDef);
+    var nomePasseggero = document.getElementById("nome-passeggero").value;
+    var km = document.getElementById("km").value;
+    var age = document.getElementById("age").value;
+    var price = 0.21 * km;
+    var sconto = 0;
+    if (age == "minorenne")
+    {
+      sconto = price * 0.2;
+    }
+    else if (age == "maggiorenne")
+    {
+      sconto = 0;
+    }
+    else
+    {
+      sconto = price * 0.4;
+    }
+    var priceDef = price - sconto;
+    var coach = Math.floor(Math.random() * (100000 - 90000) )
+    var codiceCp = Math.floor(Math.random() * (100000 - 90000) ) + 90000; //num compreso tra 90000 a 100000 escluso
+    document.getElementById("box-genera").style.display = "block";
+    document.getElementById("ticket-result").style.display = "block";
+    document.getElementById("ris-nom").innerHTML = nomePasseggero;
+    document.getElementById("offerta").innerHTML = age;
+    document.getElementById("carrozza").innerHTML = coach;
+    document.getElementById("costo-biglietto").innerHTML = priceDef;
+    document.getElementById("codice-cp").innerHTML = codiceCp;
   }
   );
